846bf7298c4d99793de44f00e0e18075fee2b744
Maxim Timofeev <hallo.bonjour.salut@gmail.com>
2022-07-08 17:01:52 +0300

    fix InputLabel z-index

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,28 +2,43 @@ import { AppHeader } from '../AppHeader';
 import { CounterDemo } from '../CounterDemo';
 import { DesktopDatePickerDemo } from '../DesktopDatePickerDemo';
 import { MultiselectDemo } from '../MultiselectDemo';
+import { createTheme, ThemeProvider } from '@mui/material';
 import styles from './App.module.scss';
+
+const theme = createTheme({
+  components: {
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          zIndex: -1,
+        },
+      },
+    },
+  },
+});
 
 export const App = () => {
   return (
-    <div className={styles.app}>
-      <AppHeader />
-      <main className={styles.main}>
-        <section className={styles.section}>
-          <h2>Date picker</h2>
-          <DesktopDatePickerDemo />
-        </section>
+    <ThemeProvider theme={theme}>
+      <div className={styles.app}>
+        <AppHeader />
+        <main className={styles.main}>
+          <section className={styles.section}>
+            <h2>Date picker</h2>
+            <DesktopDatePickerDemo />
+          </section>
 
-        <section className={styles.section}>
-          <h2>Multiselect</h2>
-          <MultiselectDemo />
-        </section>
+          <section className={styles.section}>
+            <h2>Multiselect</h2>
+            <MultiselectDemo />
+          </section>
 
-        <section className={styles.section}>
-          <h2>Counter</h2>
-          <CounterDemo />
-        </section>
-      </main>
-    </div>
+          <section className={styles.section}>
+            <h2>Counter</h2>
+            <CounterDemo />
+          </section>
+        </main>
+      </div>
+    </ThemeProvider>
   );
 };
