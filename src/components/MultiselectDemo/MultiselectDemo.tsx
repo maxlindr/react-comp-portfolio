ecf8e1d86ed77ed1550722062300b746189758b3
Maxim Timofeev <hallo.bonjour.salut@gmail.com>
2023-02-22 19:04:55 +0300

    Multiselect: display selected items

--- src/components/MultiselectDemo/MultiselectDemo.tsx
+++ src/components/MultiselectDemo/MultiselectDemo.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { findNodeById } from '../Multiselect/components/TreeView/utils';
 import { InputLabel } from '@mui/material';
 import { Multiselect } from '../Multiselect';
 import { data } from './data';
@@ -7,9 +8,21 @@ import styles from './MultiselectDemo.module.scss';
 
 export const MultiselectDemo = () => {
   const [checked, setChecked] = useState<string[]>([]);
+  const checkedItems = checked.map(
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    (id) => findNodeById(id, data)!
+  );
 
   return (
     <div className={styles.root}>
+      <ul className={styles.list}>
+        {checkedItems.map((item) => (
+          <li key={item.id} className={styles.item}>
+            {item.name}
+          </li>
+        ))}
+      </ul>
+
       <div className={styles.fieldset}>
         <InputLabel>Коктейли для ценителей</InputLabel>
         <Multiselect
